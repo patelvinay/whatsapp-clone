e2874c7bca76598cef3e6ae11300bf935a665ba7
Vinay Patel <pate0586@algonquinlive.com>
2020-09-13 09:10:16 -0700

    initialize

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtcsr8Fy615VXyf9MEoWMYPpIp3Kb2nac",
+  authDomain: "whats-app-colne-77b5d.firebaseapp.com",
+  databaseURL: "https://whats-app-colne-77b5d.firebaseio.com",
+  projectId: "whats-app-colne-77b5d",
+  storageBucket: "whats-app-colne-77b5d.appspot.com",
+  messagingSenderId: "162948037031",
+  appId: "1:162948037031:web:530cfa715bd1a1f22dde82",
+  measurementId: "G-HYVDWV92WV",
+};
